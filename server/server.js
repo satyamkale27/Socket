b1ae2ffb92727d85b8f8255d79d647b12c4d82f1
satyam <satyamkale271@gmail.com>
2025-01-30 18:54:50 +0530

    remove emitter

--- server/server.js
+++ server/server.js
@@ -3,19 +3,14 @@ const { Server } = require("socket.io");
 const httpServer = createServer();
 const socket = new Server(httpServer, {
   cors: {
-    origin: "http://127.0.0.1:5500",
+    origin: "http://localhost:5173/",
   },
 });
 
 socket.on("connection", (socket) => {
   console.log(socket);
-
-  socket.on("message", (data) => {
-    console.log(data);
-  });
-  socket.emit("message", "Hello");
 });
 
 httpServer.listen(3000, () => {
-  //   console.log("server is connected");
+  console.log("server is connected");
 });
